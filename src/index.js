ac32d2079423418d6cfa96734caf1277aaa5d92b
Kyungjin Josiah Park <KyungjinJPark@gmail.com>
2020-05-26 15:18:42 -0400

    added deck display

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+
+import './app.css';
 
 const clog = (toLog) => {
   console.log(toLog);
@@ -13,9 +16,8 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      counter: 0
+      deck: [],
     };
-    this.deck = [];
     this.fillDeck();
     this.p1Hand = [];
     this.p2Hand = [];
@@ -27,26 +29,38 @@ class App extends React.Component {
    * fills the dek with 52 base cards (no joker, sorry)
    */
   fillDeck = () => {
-    const suits = ["spades", "clubs", "diamonds", "hearts"];
+    const tempDeck = [];
+    const suits = ['spades', 'clubs', 'diamonds', 'hearts'];
     const cards = [...Array(13).keys()]; //0: two ... 12: ace
     suits.forEach((suit) => {
       cards.forEach((number) => {
-        this.deck.push(new PlayingCard(suit, number));
+        tempDeck.push(new PlayingCard(number, suit));
       })
+    });
+    this.setState(prevState => {
+      return {deck: tempDeck};
     });
   }
 
   shuffleDeck = () => {
-    this.deck.sort(() => Math.random() - 0.5);
+    const tempDeck = this.state.deck;
+    tempDeck.sort(() => Math.random() - 0.5);
+    this.setState(prevState => {
+      return {deck: tempDeck};
+    });
   }
 
   splitDeck = () => {
     this.shuffleDeck();
-    const deckSize = this.deck.length;
+    const deckSize = this.state.deck.length;
     for (let i = 0; i < Math.ceil(deckSize / 2); i++) {
-      this.p1Hand.push(this.deck.pop());
-      this.p2Hand.push(this.deck.pop());
+      this.p1Hand.push(this.state.deck.pop());
+      this.p2Hand.push(this.state.deck.pop());
     }
+    //ingores flexibility
+    this.setState(prevState => {
+      return {deck: []};
+    });
   }
 
   playTurn = () => {
@@ -65,20 +79,27 @@ class App extends React.Component {
 
   render() {
     //how to make this run once?
-    if (this.temp) {
-      this.temp = false;
-      clog(this.deck);
-      this.shuffleDeck();
-      clog(this.deck);
-      this.splitDeck();
-      clog(this.p1Hand);
-      clog(this.p1Hand);
-    }
 
     return (
-      <Container className="woot">
-        <h1>This is War</h1>
-        <button onClick={this.playTurn}>play turn</button>
+      <Container>
+        {/* <div className='woot'> */}
+          <h1>This is War</h1>
+          <button onClick={this.fillDeck}>fill deck</button>
+          <button onClick={this.shuffleDeck}>shuffle deck</button>
+          <button onClick={this.splitDeck}>split deck</button>
+          <button onClick={this.playTurn}>play turn</button>
+        {/* </div> */}
+        <div className="deck">
+          {this.state.deck.map((card) => {
+            return <PlayingCardDisplay num={card.colloqNum()} suit={card.suit}/>
+          })}
+        </div>
+        {/* <div className="p1hand">
+          <PlayingCardDisplay />
+        </div>
+        <div className="p2hand">
+          <PlayingCardDisplay />
+        </div> */}
       </Container>
     )
   }
@@ -90,12 +111,13 @@ class App extends React.Component {
 class PlayingCard {
   /**
    * creates a card
-   * @param {String} suit 
    * @param {Number} num 0: two ... 12: ace
+   * @param {String} suit 
    */
-  constructor(suit, num) {
-    this.suit = suit;
+  constructor(num, suit) {
+    this.colloquialNumbers = [2,3,4,5,6,7,8,9,10,"jack","queen","king","ace"];
     this.num = num;
+    this.suit = suit;
   }
 
   /**
@@ -106,9 +128,30 @@ class PlayingCard {
     return this.num - other.num;
   }
 
-  toString = () => {
-    return `${this.num + 2} of ${this.suit}`
+  colloqNum = () => {
+    return this.colloquialNumbers[this.num];
   }
+
+  toString = () => {
+    return `${this.colloqNum} of ${this.suit}`
+  }
+}
+
+const PlayingCardDisplay = (props) => {
+  const {num, suit} = props;
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{num}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">of {suit}</Card.Subtitle>
+        {/* <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text> */}
+      </Card.Body>
+    </Card>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
